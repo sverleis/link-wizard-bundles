aeb9075d722933fd204ecac40ef50e8d2d91740c
Sven Leisegang <sven.leisegang@automattic.com>
2025-10-29 21:05:14 +0200

    fix: add link-wizard-for-woocommerce to RequiresPlugins header\n\nThis ensures the addon is detected by Link Wizard's addon manager and appears in AddonsSection

--- link-wizard-bundles.php
+++ link-wizard-bundles.php
@@ -10,7 +10,7 @@
  * Domain Path: /languages
  * Requires at least: 6.2
  * Requires PHP: 7.4
- * Requires Plugins: woocommerce
+ * Requires Plugins: woocommerce, link-wizard-for-woocommerce
  * License: GPL-3.0-or-later
  */
 
